a6b97a2bfd6b9bae36b1cd06d5e431dcc8e40a2c
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2021-10-12 12:58:22 -0500

    Se realizaron varias pruebas con los metodos agrega, remueva y notifica de la clase thisney. Por el momento funcionan como deberian excepto porque notifica aun no remueve clientes cuando ya no tienen dinero

--- Main.java
+++ Main.java
@@ -4,44 +4,77 @@ public class Main{
 
 	ThisneyPlus thisney = new ThisneyPlus();
 	Persona alicia = new Persona("alicia ", 1000);
+	Persona jake = new Persona("jake",  300);
 	thisney.agrega(alicia, 1);
+	thisney.agrega(jake, 1);
 	thisney.mostrarClientesActivos();
 
 	System.out.println();
 	System.out.println("mes 1");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 	thisney.notifica();
 	System.out.println("realizamos el cobro");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 
 	System.out.println();
 	System.out.println("mes 2");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 	thisney.notifica();
 	System.out.println("realizamos el cobro");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 
 	System.out.println();
 	System.out.println("mes 3");
+	thisney.remueve(jake);
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 	thisney.notifica();
 	System.out.println("realizamos el cobro");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 
 	System.out.println();
 	System.out.println("mes 4");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
 	thisney.notifica();
 	System.out.println("realizamos el cobro");
 	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
 	System.out.println(alicia);
+	System.out.println(jake);
+
+	System.out.println();
+	System.out.println("mes 5");
+	thisney.agrega(jake, 1);
+	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
+	System.out.println(alicia);
+	System.out.println(jake);
+	thisney.notifica();
+	System.out.println("realizamos el cobro");
+	thisney.mostrarClientesActivos();
+	thisney.mostrarExClientes();
+	System.out.println(alicia);
+	System.out.println(jake);
     }
     
 }
